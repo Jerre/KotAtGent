f1917c2de5b1b7a9830e35a180949ee0902c4c59
haiQt <thomas.tuts@gmail.com>
2012-07-17 10:49:49 +0200

    Initial map coding

--- prototype/js/libs/jtrace.js
+++ prototype/js/libs/jtrace.js
@@ -5,10 +5,11 @@
 		this.element.type = 'text/plain';
 		this.element.style.background = 'black';
 		this.element.style.bottom = '0px';
+		this.element.style.right = '0px';
 		this.element.style.color = 'white';
 		this.element.style.display = 'block';
 		this.element.style.fontFamily = 'monospace';
-		this.element.style.height = '40px';
+		this.element.style.height = '100%';
 		this.element.style.opacity = '0.5';
 		this.element.style.overflow = 'auto';
 		this.element.style.position = 'fixed';
@@ -16,14 +17,14 @@
 		this.element.style.webkitTransition = 'opacity 0.1s linear, bottom 0.1s ease-in';
 		this.element.style.mozTransition = 'opacity 0.1s linear, bottom 0.1s ease-in';
 		this.element.style.whiteSpace = 'pre';
-		this.element.style.width = '100%';
+		this.element.style.width = '400px';
 		this.element.style.zIndex = '1';
 		this.element.onmouseover = function() {
-			this.style.height = '80px';
+			this.style.height = '100%';
 			this.style.opacity = '0.9';
 		}
 		this.element.onmouseout = function() {
-			this.style.height = '40px';
+			this.style.height = '100%';
 			this.style.opacity = '0.5';
 		}
 		document.head.appendChild(this.element);
--- prototype/js/script.js
+++ prototype/js/script.js
@@ -1,53 +1,129 @@
-var day = 14;
+var addressList = new Array();
+var latlngList = new Array();
+var objAddress = {
+    lat: 1,
+    long: 1
+};
+
+var index = 0;
+
+/*
+* ===================================
+* LOAD JSON DATA FROM TDT
+* ===================================
+*/
 
 $.ajax({
     url : 'http://data.appsforghent.be/kotatgent/prototype.json',
     async : false,
     dataType : 'json',
     success : function(data) {
-        cbSuccess(data)
+        cbSuccess(data);
     }
 });
 
 function cbSuccess(data) {
-    // trace(data.prototype);
-
-     $.each(data.prototype, function(key, val) {
-        trace(val.Zone);
-      });
+    $.each(data.prototype, function(key, val) {
+        addressList.push(val.Adres + ", Gent");
+        trace("pushed to array");
+        trace(addressList.length);
+    });
+    getLatLongList();
+    parseToDebugList(data);
+    renderAllToMap(data);
 }
 
-var geocoder;
-  var map;
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(-34.397, 150.644);
-    var myOptions = {
-      zoom: 8,
-      center: latlng,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-  }
+/*
+* ===================================
+* INITIALIZE MAP
+* ===================================
+*/
 
-  function codeAddress(inputAddress) {
-    // var address = document.getElementById("address").value;
-    var address = inputAddress;
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: map, 
-            position: results[0].geometry.location
-        });
-      } else {
-        alert("Geocode was not successful for the following reason: " + status);
-      }
-    });
+var geocoder;
+var map;
+
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var myOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   }
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+
+/*
+* ===================================
+* GEOCODE LATLONG FROM ADDRESS
+* ===================================
+*/
+
+function codeAddress(inputAddress) {
+  // var address = document.getElementById("address").value;
+  var address = inputAddress;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+      map: map, 
+      position: results[0].geometry.location
+    });
+    } else {
+      alert("Geocode was not successful for the following reason: " + status);
+    }
+  });
+}
+
+function getLatLongList() {
+  // var address = document.getElementById("address").value;
+  for (var i = addressList.length - 1; i >= 0; i--) {
+    trace("hello");
+  };
+}
+
+/*
+* ===================================
+* RENDER ALL MARKERS ON MAP
+* ===================================
+*/
+
+function renderAllToMap()
+{
+    $.each(data.prototype, function(key, val) {
+        var item = val.Adres + ", Gent";
+        // trace("Rendering: " + item);
+
+    });
+}
+
+/*
+* ===================================
+* DEBUG METHODS
+* ===================================
+*/
+
+/* LOAD DATA INTO DEBUG LIST */
+
+function parseToDebugList(data)
+{
+    $.each(data.prototype, function(key, val) {
+        var item = val.Adres + ", Gent";
+        $("#debuglist").append("<li>" + item + "</li>");
+    });
+}
+
+/* GEOCODE ADDRESS */
+
+$("#geocode").click(function() {
+    var input = document.getElementById("address").value;
+    trace("geocoding --- " + input + ", Gent");
+    codeAddress(input);
+});
+
+/* DEBUG */
 
 $("#debug").click(function() {
-  var input = document.getElementById("address").value;
-  trace("geocoding --- " + input);
-  codeAddress(input);
+    trace(objAddress.lat);
+    trace(addressList[0]);
 });
