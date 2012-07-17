c259448025d482cceddff1776ad9591632b2e0ca
Thomas Tuts <thomas.tuts@gmail.com>
2012-07-17 22:29:19 +0200

    Colored marker for every type of residency

--- prototype/js/render.js
+++ prototype/js/render.js
@@ -11,21 +11,57 @@ function renderAll(data)
 			shadowSize : null,
 			iconAnchor : new L.Point(16, 35)});
 			
-	var Koten = L.Icon.extend({
-			iconUrl : 'images/marker_event.png',
+	var Kamer = L.Icon.extend({
+			iconUrl : 'images/marker_kamer.png',
 			iconSize : new L.Point(25, 41),
 			shadowSize : null,
 			iconAnchor : new L.Point(16, 35)});
+
+	var Studio = L.Icon.extend({
+			iconUrl : 'images/marker_studio.png',
+			iconSize : new L.Point(25, 41),
+			shadowSize : null,
+			iconAnchor : new L.Point(16, 35)});
+
+	var Appartement = L.Icon.extend({
+			iconUrl : 'images/marker_appartement.png',
+			iconSize : new L.Point(25, 41),
+			shadowSize : null,
+			iconAnchor : new L.Point(16, 35)});
+
+	var Woning = L.Icon.extend({
+			iconUrl : 'images/marker_woning.png',
+			iconSize : new L.Point(25, 41),
+			shadowSize : null,
+			iconAnchor : new L.Point(16, 35)});
+
 	var group = new L.LayerGroup();
 	
 	var markerOwnLocation = new L.Marker(new L.LatLng(51.050729,3.724022));
 	markerOwnLocation.setIcon(new MyIcon);
-	markerOwnLocation.bindPopup("test");
+	markerOwnLocation.bindPopup("U bevindt zich hier");
 	map.addLayer(markerOwnLocation);
 	$.each(data.data, function(key, value){
+
 		var markerKot = new L.Marker(new L.LatLng(value.Lat,value.Long));
-		markerKot.setIcon(new Koten);
-		markerKot.bindPopup("test");
+
+		switch(value.Type)
+		{
+			case "Kamer":
+				markerKot.setIcon(new Kamer);
+				break;
+			case "Studio":
+				markerKot.setIcon(new Studio);
+				break;
+			case "Appartement":
+				markerKot.setIcon(new Appartement);
+				break;
+			case "Woning":
+				markerKot.setIcon(new Woning);
+				break;
+		}
+		
+		markerKot.bindPopup(value.Type + " - " + value.Adres);
 		group.addLayer(markerKot);
 	});
 
