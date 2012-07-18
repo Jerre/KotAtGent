9a52eef82f2593e23b6e05b32a440337c01e5e5f
Thomas Tuts <thomas.tuts@gmail.com>
2012-07-18 14:53:37 +0200

    Back button for details page, full info shown

--- prototype/js/details.js
+++ prototype/js/details.js
@@ -8,19 +8,40 @@ function getDetails(id)
 
             fadePanels();
 
+            $("#details").html("");
+
             var htmlContent;
-            htmlContent =   "<h3>" + value.Adres + "</h3>" + "<h3>" + value.Zone + "</h3>" + 
+            htmlContent =   "<a href='#' class='back' onClick='backToMap()'>Terug naar map</a>" +
+                            "<h3>" + value.Adres + "</h3>" + "<h3>" + value.Zone + "</h3>" + 
                             "<div id='details_typeprice' class='container'>" + 
                                 "<div class='container_left'>" + 
                                     value.Type + 
                                 "</div>" + 
                                 "<div class='container_right'>" +
-                                    value.Max_prijs +
+                                    "&euro;" + value.Max_prijs +
                                     "</div>" + 
                                 "</div>" +
+                            "</div>" +
+
+                            "<div id='details_note' class='container'>" +
+                                "<h4>Opmerking</h4>" +
+                                "<textarea />" +
+                            "</div>" +
+
+                            "<div id='details_contact' class='container'>" +
+                                "<h4>Contact</h4>" +
+                                "<ul>" +
+                                "<li>" + value.Eigenaar_naam + "</li>" +
+                                "<li>" + value.Eigenaar_telefoon + "</li>" +
+                                "<li>" + value.Eigenaar_email + "</li>" +
+                                "</ul>" +
                             "</div>"
 
                             ;
+
+        
+
+        
 
 
             $("#details").html(htmlContent);
@@ -29,3 +50,4 @@ function getDetails(id)
         }
     });
 }
+
